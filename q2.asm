b4bd40a5dd5bec7851d99fb85597c946cd3ea0ca
BenEf97 <78364069+BenEf97@users.noreply.github.com>
2022-12-29 20:35:18 +0200

    Turn Q2 to use arguments, Finished Q3.

--- q2.asm
+++ q2.asm
@@ -6,25 +6,26 @@
 #The caller needs to assign space
 #Note: need to enable direct access in MARS in order to work. To do that follow the instructions below:
 #Open MARS->Click on 'Settings' tab->Check on "Assemble all files in directory".
-.data 0x10010000
-Array: .space 80 # init Array[80 bytes]==Array[20 words].
+.data 
 .globl Data_Block #access from directory folder
 
 .text
 Data_Block:
-#Check for arguments, if they are all zero then continue normally.
-li $t0,20 #$t0=20
-la $t1,Array #load address of Array to $t1.
+addi $sp,$sp,-4
+sw $s0, 0($sp)
+add $t0,$a0,$0 #$t0=$a0, means that $t0 has the start of the block address
+add $t1,$a1,$0 #$t1=$a1, means that $t1 has the number of words.
 L1:
-beq $t0,$0,END #if $t0==0 then jump to the END.
+beq $t1,$0,END_DataBlock #if $t1==0 then jump to the END.
 li $v0,42 #call for rand function.
 li $a1,100 #sets $a0 to be in the range of 0<=$a0<=100.
 syscall #$a0=Rand in the given range (0-100).
 addi $a0,$a0,-50 #$a0-=50 to change the range -50<=$a0<=50.
-sw $a0,0($t1) #$t1=$a0, store the random number in the array
-addi $t1,$t1,4 #$t1+=4, go to next index in the array.
-addi $t0,$t0,-1 #$t0-=1
+sw $a0,0($t0) #$t0=$a0, store the random number in the array
+addi $t0,$t0,4 #$t0+=4, go to next index in the array.
+addi $t1,$t1,-1 #$t1-=1
 j L1 #jump to L1 label
-END:
-la $t0,Array #load the desired address to $t0, save it later if needed
+END_DataBlock:
+lw $s0, 0($sp)
+addi $sp, $sp, 4
 jr $ra #return to main
