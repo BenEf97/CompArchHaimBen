e4a4db9179e846b04f0e3f888feed72f9a174370
BenEf97 <78364069+BenEf97@users.noreply.github.com>
2022-12-30 08:04:03 +0200

    finished q11,q12

--- q11.asm
+++ q11.asm
@@ -1,4 +1,4 @@
-#Q10: Sum a[i]=a[i]+a[i+1], i=0,2,4...
+#Q11: Count all the positive numbers
 .data 0x10010000
 Array: .space 80 #init Array[80 bytes]==Array[20 words]
 .text 
@@ -6,16 +6,21 @@ la $a0,Array #$a0=Address of Array
 li $a1,20 #$a1=20 words
 jal Data_Block #init data block
 la $t0,Array #$t0=0x10010000
-li $t2,10 #$t2=10 idx
+li $t2,20 #$t2=20 idx
+li $t1,0 #t1=0 counter
 Loop:
 beq $t2,$0,END #if $t2==0 jump to END
 lw $t3,0($t0) #$t3=$t0[idx]
-lw $t4,4($t0) #$t4=$t0[idx+1]
-add $t3,$t3,$t4 #$t3+=$t4
-sw $t3,0($t0) #$t0[idx]=$t3
+slt $t4,$0,$t3 # if 0<$t3 then $t4
+beq $t4,$0,Update #if $t4==0 then jump to update, else increase the count
+addi $t1,$t1,1 #$t1++ increase counter
+Update:
 addi $t2,$t2,-1 #$t2--
-addi $t0,$t0,8 #$t0[idx+2]
+addi $t0,$t0,4 #$t0[idx++]
 j Loop
 END:
+li $v0,1 #print result
+move $a0,$t1 
+syscall
 li $v0,10 #terminate program
 syscall
--- q12.asm
+++ q12.asm
@@ -1,4 +1,4 @@
-#Q10: Sum a[i]=a[i]+a[i+1], i=0,2,4...
+#Q12: Multiply all the data by 2.
 .data 0x10010000
 Array: .space 80 #init Array[80 bytes]==Array[20 words]
 .text 
@@ -6,15 +6,15 @@ la $a0,Array #$a0=Address of Array
 li $a1,20 #$a1=20 words
 jal Data_Block #init data block
 la $t0,Array #$t0=0x10010000
-li $t2,10 #$t2=10 idx
+li $t2,20 #$t2=20 idx
+li $t1,2 #$t1=2
 Loop:
 beq $t2,$0,END #if $t2==0 jump to END
 lw $t3,0($t0) #$t3=$t0[idx]
-lw $t4,4($t0) #$t4=$t0[idx+1]
-add $t3,$t3,$t4 #$t3+=$t4
+mul $t3,$t3,$t1 #$t3*=2
 sw $t3,0($t0) #$t0[idx]=$t3
 addi $t2,$t2,-1 #$t2--
-addi $t0,$t0,8 #$t0[idx+2]
+addi $t0,$t0,4 #$t0[idx++]
 j Loop
 END:
 li $v0,10 #terminate program
